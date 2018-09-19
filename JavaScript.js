0fe7ebc7a7188cf3a4a395a5b8c5237354ef5ea6
Raghavendrajvs <43164756+Raghavendrajvs@users.noreply.github.com>
2018-09-19 10:12:48 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+﻿alert("hai raghu");
+console.log("hello");
+
+
+//$("body").css("background", "green");
+
+$(document).ready(
+    function() {
+        $("body").css("background", "green");
+        $(".h1").css("color", "white");
+        $("#h2").css("background - color", "red", "width", "500px");
+                }
+               )
---
+++ example.js
@@ -0,0 +1,19 @@
+﻿alert("you need to see this");
+console.log("is that necessary?");
+
+//$("p").addClass("paragraph");
+//$(document).ready({
+//    function() {
+//        $("button").click(function () {
+//            $("p:first").addClass("intro");
+//        }
+//    }
+//}
+//)
+$(document).ready(function () {
+    $("button").click(function () {
+        //$("p:first").removeClass("ducks").addClass("intro");
+        //alert($("p").length);
+        $(":header").css("background-color", "yellow");
+    });
+});
